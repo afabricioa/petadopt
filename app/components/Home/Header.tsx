e166ce69d4d799571428861cf4b0d33457943cbb
Fabrício <fabricio.silva@irede.org.br>
2025-03-14 16:27:22 -0300

    created login using google and a header

---
+++ app/components/Home/Header.tsx
@@ -0,0 +1,42 @@
+import { View, Text, Image } from 'react-native'
+import React from 'react'
+import { useUser } from '@clerk/clerk-expo'
+
+export default function Header() {
+    const { user } = useUser();
+  
+    return (
+        <View
+            style={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center'
+            }}
+        >
+            <View>
+                <Text
+                    style={{
+                        fontSize: 18
+                    }}
+                >
+                    Bem-vindo,
+                </Text>
+                <Text
+                    style={{
+                        fontSize: 24
+                    }}
+                >
+                    {user?.fullName}
+                </Text>
+            </View>
+            <Image source={{uri: user?.imageUrl}} 
+                style={{
+                    width: 40,
+                    height: 40,
+                    borderRadius: 99
+                }}
+            />
+        </View>
+    )
+}
